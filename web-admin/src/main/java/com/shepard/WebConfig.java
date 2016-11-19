570d73957c279be9bde803a045cdf37fde00dcc0
shepard.xia <xia8910168@hotmail.com>
2016-11-19 14:19:24 +0800

    建立独立模块

---
+++ web-admin/src/main/java/com/shepard/WebConfig.java
@@ -0,0 +1,8 @@
+package com.shepard;
+
+/**
+ * Created by IonCannon on 2016/11/19.
+ */
+
+public class WebConfig {
+}
---
+++ web-admin/src/main/java/com/shepard/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.shepard.controller;
+
+/**
+ * Created by IonCannon on 2016/11/19.
+ */
+
+public class DemoController {
+}
