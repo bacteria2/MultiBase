e0cf10a8da86808002678b05e2a9910494494c78
shepard.xia <xia8910168@hotmail.com>
2016-11-19 12:26:03 +0800

    建立独立模块

---
+++ common/src/main/java/util/DemoTest.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created by IonCannon on 2016/11/19.
+ */
+public class DemoTest {
+    public String getSomeThing(){
+         return "getSomething worked";
+    }
+}
---
+++ dataCenter/src/main/java/demo/service/RunDemoTest.java
@@ -0,0 +1,14 @@
+package demo.service;
+
+import util.DemoTest;
+
+/**
+ * Created by IonCannon on 2016/11/19.
+ */
+public class RunDemoTest {
+    public static void main(String[] args) {
+        DemoTest demoTest=new DemoTest();
+        System.out.println(demoTest.getSomeThing());;
+
+    }
+}
