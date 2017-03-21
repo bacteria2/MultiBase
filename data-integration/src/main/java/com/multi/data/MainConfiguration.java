aa661077a5118038df230a8e7f8bb5349b9f2a25
shepard <xia8910168@Hotmail.com>
2017-03-21 13:57:00 +0800

    修改配置

--- data-integration/src/main/java/com/multi/data/MainConfiguration.java
+++ data-integration/src/main/java/com/multi/data/MainConfiguration.java
@@ -1,12 +1,20 @@
 package com.multi.data;
 
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
- * @author shepard.xia
+ * @author multi.xia
  * @date 2017年03月16日
  * @description input useage
  */
@@ -15,8 +23,43 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 public class MainConfiguration {
 
     @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
-        return new PropertySourcesPlaceholderConfigurer();
+    public  PathMatchingResourcePatternResolver resolver(){
+        return new PathMatchingResourcePatternResolver();
     }
 
+    /**
+     * 注入属性for development
+     * */
+    @Bean(name="propertiesFactoryBean")
+    @Profile("development")
+    public PropertiesFactoryBean devPropertiesFactoryBean(PathMatchingResourcePatternResolver resolver) throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean=new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocations(
+                resolver.getResource("classpath:db_dev.properties")
+        );
+        return propertiesFactoryBean;
+    }
+
+    /**
+     * 注入属性for production
+     * */
+    @Bean(name="propertiesFactoryBean")
+    @Profile("production")
+    public PropertiesFactoryBean prodPropertiesFactoryBean(PathMatchingResourcePatternResolver resolver) throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean=new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocations(
+                resolver.getResource("classpath:db_prod.properties")
+        );
+        return propertiesFactoryBean;
+    }
+
+    /**
+     * 解析形如 @Value("${dataSource.className}")
+     * */
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(PropertiesFactoryBean factoryBean) throws IOException {
+        PropertySourcesPlaceholderConfigurer configurer= new PropertySourcesPlaceholderConfigurer();
+        configurer.setProperties(factoryBean.getObject());
+        return configurer;
+    }
 }
--- data-integration/src/main/java/com/multi/data/conf/MybatisConfiguration.java
+++ data-integration/src/main/java/com/multi/data/conf/MybatisConfiguration.java
@@ -2,34 +2,63 @@ package com.multi.data.conf;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+
 /**
- * @author shepard.xia
+ * @author multi.xia
  * @date 2017年03月16日
  * @description input useage
  */
 @Configuration
-@PropertySource("db.properties")
+@EnableTransactionManagement
+//@PropertySource("db.properties")
 public class MybatisConfiguration {
 
     @Autowired
+    @Qualifier("propertiesFactoryBean")
     private Properties dbEnvironment;
 
+    @Value("${dataSource.url}")
+    private String a;
 
     @Bean
-    public DataSource dataSource(){
-
-
-        HikariConfig config=new HikariConfig(dbEnvironment);
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(dbEnvironment.getProperty("dataSource.className"));
+        config.setJdbcUrl(dbEnvironment.getProperty("dataSource.url"));
+        config.setUsername(dbEnvironment.getProperty("dataSource.user"));
+        config.setPassword(dbEnvironment.getProperty("dataSource.password"));
+        config.setMaximumPoolSize(Integer.valueOf(dbEnvironment.getProperty("dataSource.maximumPoolSize")));
         return new HikariDataSource(config);
     }
+
+    @Bean
+    public SqlSessionFactory sessionFactoryBean(DataSource dataSource,PathMatchingResourcePatternResolver resolver ) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean= new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:/mapper/*.xml"));
+        return  sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
 }
